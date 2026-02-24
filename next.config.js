57571cd304ef80c0a53609e5f43e418afcd45281
pacing030 <pacing030@gmail.com>
2026-02-24 13:36:53 +0100

    Add TypeScript config to ignore build errors

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   assetPrefix: process.env.BASE_PATH || "",
   basePath: process.env.BASE_PATH || "",
   trailingSlash: true,
